3962f47093e5bf4cebe07aa26e28d35e9ea49a3c
zhenbing <zjh@cnic.cn>
2018-02-23 15:43:02 +0800

    point temporal query

--- metadata/landsat_tile_search.py
+++ metadata/landsat_tile_search.py
@@ -61,6 +61,55 @@ def spatial_temporal_query_test():
         print(grid['_source'].keys())
         print(grid['_source']['date_acquired'])
 
+def point_temporal_query_test():
+    query_point = {'type': 'Point', 'coordinates': [112, 22]}
+    start_time = "20010117"
+    end_time = "20150101"
+    st_filter = {
+        "query": {
+            "bool": {
+                "filter": [
+                    {"geo_shape": {
+                        "wgs_grid": {
+                            "shape": query_point,
+                            "relation": "contains"
+                        }
+                    }},
+                    {'range':{'date_acquired':{'gt':start_time, 'lte':end_time, "format":"yyyyMMdd||yyyy"}}}
+
+                ]
+            }
+        }
+    }
+
+    # filter = {
+    #     "query": {
+    #         "bool": {
+    #             "filter": {
+    #                 "geo_shape": {
+    #                     "wgs_grid": {
+    #                         "shape": query_polygon,
+    #                         "relation": "intersects"
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     }
+    # }
+
+    # query = json.dumps(filter)
+    query = json.dumps(st_filter)
+
+    res = es.search(index="landsat_tiles", body=query, size=1000)
+    print(res['hits']['total'])
+    tile_dict = (res['hits']['hits'])
+
+    for grid in tile_dict:
+        print(grid['_source']['tile_path'], grid['_source']['wgs_grid'])
+        print(grid['_source'].keys())
+        print(grid['_source']['date_acquired'])
+
 if __name__ == '__main__':
-    spatial_temporal_query_test()
+    # spatial_temporal_query_test()
+    point_temporal_query_test()
 
